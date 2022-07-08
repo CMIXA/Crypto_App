b57cd691bccfb9e0b9e8f03d87e922331b2a6c52
Chris Mixa <91347624+CMIXA@users.noreply.github.com>
2022-07-08 16:19:31 -0400

    Add files via upload

--- src/components/Crypto_Row.js
+++ src/components/Crypto_Row.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Crypto_Row = ({rank, image, name, ticker, price, change24h_p, change24h_$, market_cap}) => {
+    const index = 1;
     return (
         <div className='crypto-container'>
             <div className='crypto-wrapper'>
@@ -9,7 +10,17 @@ const Crypto_Row = ({rank, image, name, ticker, price, change24h_p, change24h_$,
                     <img src={image}/>
                     <p className='crypto-name'>{ticker} - {name}</p>
                     <p className='crypto-price'>${price}</p>
-                    <p className='crypto-change'>{change24h_p}% | ${change24h_$}</p>
+                    {/* Wrap in DIV */}
+                    {change24h_p <= 0 ? (
+                        <p className='crypto-pchange-negative'> {change24h_p}%</p>
+                    ) : (
+                        <p className='crypto-pchange-positive'> +{change24h_p}%</p>
+                    )}
+                    {change24h_$ <= 0 ? (
+                        <p className='crypto-dchange-negative'> -{change24h_$.substring(1, index) + '$' + change24h_$.substring(index)}</p>
+                    ) : (
+                        <p className='crypto-dchange-positive'> +${change24h_$}</p>
+                    )}
                     <p className='crypto-cap'>${market_cap}</p>
                 </div>
             </div>
